b623e6bd45ae1d3aa0084d5036f345446ad16338
Albinator <albinamivina@gmail.com>
2023-06-09 11:29:15 +0300

    handling get request for tours

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.status(200).send('Hello from the server side');
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'));
+
+app.get('/api/v1/tours', (req, res) => {
+	if (tours) res.status(200).json({ status: 'success', data: { tours } });
 });
 
 const port = 3000;
