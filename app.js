32a02fc67a890d436437b2002e4f888df1cb3a85
Albinator <albinamivina@gmail.com>
2023-06-09 11:31:01 +0300

    add array length to response

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express();
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours.json'));
 
 app.get('/api/v1/tours', (req, res) => {
-	if (tours) res.status(200).json({ status: 'success', data: { tours } });
+	if (tours)
+		res.status(200).json({
+			status: 'success',
+			results: tours.length,
+			data: { tours },
+		});
 });
 
 const port = 3000;
