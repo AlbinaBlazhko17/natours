8fb5ed85fdab856c9689c01ccb4e4604731a2729
Albinator <albinamivina@gmail.com>
2023-06-15 11:21:13 +0300

    connect to db

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
 import app from './app.js';
-import { process } from 'dotenv';
 
+// eslint-disable-next-line no-undef
 const port = process.env.port || 3000;
+// eslint-disable-next-line no-undef
+const DB = process.env.DB_CONNECTION.replace('<PASSWORD>', process.env.DB_PASS);
+
+mongoose.connect(DB).then(() => {
+	console.log('DB connected successful!');
+});
 
 app.listen(port, () => {
 	console.log(`App running on port ${port}`);
