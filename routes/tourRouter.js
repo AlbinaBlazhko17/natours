235bd72a44f3de2dab5f35aa7c5cb96749d97b72
Albinator <albinamivina@gmail.com>
2023-06-16 13:18:02 +0300

    add new route

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -5,9 +5,11 @@ import {
 	getTour,
 	updateTour,
 	deleteTour,
+	aliasTopTours,
 } from '../controller/tours.controller.js';
 
 const tourRouter = express.Router();
+tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
