566f21256dbbe622b5b4b79bad95e4678b390e79
Albinator <albinamivina@gmail.com>
2023-06-16 11:01:40 +0300

    add sorting in query results

--- controller/tours.controller.js
+++ controller/tours.controller.js
@@ -26,6 +26,14 @@ export const getAllTours = async (req, res) => {
 		);
 
 		const query = Tour.find(JSON.parse(queryStr));
+
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(',').join('');
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort('-createdAt');
+		}
+
 		const allTours = await query;
 
 		res.status(200).json({
