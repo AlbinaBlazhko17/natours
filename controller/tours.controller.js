0d7915e6efa4891c39eb7513f577324bf7f23c82
Albinator <albinamivina@gmail.com>
2023-06-19 12:10:12 +0300

    refactor API features

--- controller/tours.controller.js
+++ controller/tours.controller.js
@@ -1,4 +1,5 @@
 import Tour from '../models/toursModel.js';
+import APIFeatures from '../utils/apiFeatures.js';
 
 export const aliasTopTours = (req, res, next) => {
 	req.query.limit = '5';
@@ -22,44 +23,8 @@ export const createTour = async (req, res) => {
 
 export const getAllTours = async (req, res) => {
 	try {
-		const queryObj = { ...req.query };
-		const excludedFields = ['page', 'sort', 'limit', 'fields'];
-		excludedFields.forEach((el) => delete queryObj[el]);
-
-		let queryStr = JSON.stringify(queryObj);
-		queryStr = queryStr.replace(
-			/\b(gte|gt|lte|lt)\b/g,
-			(match) => `$${match}`
-		);
-
-		const query = Tour.find(JSON.parse(queryStr));
-
-		if (req.query.sort) {
-			const sortBy = req.query.sort.split(',').join('');
-			query = query.sort(sortBy);
-		} else {
-			query = query.sort('-createdAt');
-		}
-
-		if (req.query.fields) {
-			const fields = req.query.fields.split(',').join(' ');
-			query = query.select(fields);
-		} else {
-			query = query.select('-__v');
-		}
-
-		const page = +req.query.page || 1;
-		const limit = +req.query.limit || 100;
-		const skip = (page - 1) * limit;
-
-		query = query.skip(skip).limit(limit);
-
-		if (req.query.page) {
-			const numTours = await Tour.countDocuments();
-			if (skip >= numTours) throw new Error('This page does not exist');
-		}
-
-		const allTours = await query;
+		const features = new APIFeatures(Tour.find(), req.query).filter().sort().limit().paginate();
+		const allTours = await features.query;
 
 		res.status(200).json({
 			status: 'success',
@@ -82,14 +47,10 @@ export const getTour = async (req, res) => {
 
 export const updateTour = async (req, res) => {
 	try {
-		const updatedTour = await Tour.findByIdAndUpdate(
-			req.params.id,
-			req.body,
-			{
-				new: true,
-				runValidators: true,
-			}
-		);
+		const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
 		res.status(200).json({
 			status: 'success',
 			data: { updatedTour },
