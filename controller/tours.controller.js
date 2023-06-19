4d2f76bf0f709b24900dccd19fbbec57a5f61ce4
Albinator <albinamivina@gmail.com>
2023-06-19 12:46:05 +0300

    add aggregation pipeline

--- controller/tours.controller.js
+++ controller/tours.controller.js
@@ -71,3 +71,33 @@ export const deleteTour = async (req, res) => {
 		res.status(404).json({ status: 'fail', message: 'Cannot delete tour' });
 	}
 };
+
+export const getTourStats = async (req, res) => {
+	try {
+		const stats = await Tour.aggregate([
+			{
+				$match: { ratingsAverage: { $gte: 4.5 } },
+			},
+			{
+				$group: {
+					_id: { $toUpper: '$difficulty' },
+					numTours: { $sum: 1 },
+					numRatings: { $sum: '$ratingsQuantity' },
+					avgRating: { $avg: '$ratingsAverage' },
+					avgPrice: { $avg: '$price' },
+					minPrice: { $min: '$price' },
+					maxPrice: { $max: '$price' },
+				},
+			},
+			{
+				$sort: { avgPrice: 1 },
+			},
+		]);
+		res.status(200).json({
+			status: 'success',
+			data: { stats },
+		});
+	} catch (err) {
+		res.status(404).json({ status: 'fail', message: 'Cannot get stats of tour' });
+	}
+};
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -6,10 +6,13 @@ import {
 	updateTour,
 	deleteTour,
 	aliasTopTours,
+	getTourStats,
 } from '../controller/tours.controller.js';
 
 const tourRouter = express.Router();
+
 tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+tourRouter.route('/tour-stats').get(getTourStats);
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
