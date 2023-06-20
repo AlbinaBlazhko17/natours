1d0c390bd4fd10ae02b8336a95daf37562eedf58
Albinator <albinamivina@gmail.com>
2023-06-20 10:47:44 +0300

    get monthly-plan

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -7,12 +7,14 @@ import {
 	deleteTour,
 	aliasTopTours,
 	getTourStats,
+	getMonthlyPlan,
 } from '../controller/tours.controller.js';
 
 const tourRouter = express.Router();
 
 tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 tourRouter.route('/tour-stats').get(getTourStats);
+tourRouter.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
