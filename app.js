08988f65e20597e096b314accc8e9645bbee164c
Albinator <albinamivina@gmail.com>
2023-06-20 15:18:50 +0300

    implementing a global error handling

--- app.js
+++ app.js
@@ -8,4 +8,22 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+	res.status(404).json({
+		status: 'fail',
+		message: `Can't find ${req.originalUrl} on this server!`,
+	});
+	next();
+});
+
+app.use((err, req, res, next) => {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
+	});
+});
+
 export default app;
