d622e3e9bd5866ae6011d0364b0257d0552030a6
Albinator <albinamivina@gmail.com>
2023-06-21 11:07:39 +0300

    handling errors during dev and prod

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
 import globalErrorHandler from './controller/errorController.js';
 import tourRouter from './routes/tourRouter.js';
 import userRouter from './routes/userRouter.js';
 import { AppError } from './utils/appError.js';
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
