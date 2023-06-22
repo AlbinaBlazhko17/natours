30db39a38fdca1d999df9a0fe00c6952a8ddf65c
Albinator <albinamivina@gmail.com>
2023-06-22 10:43:39 +0300

    add jwt token

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,18 @@
 import User from '../models/userModel.js';
 import catchAsync from '../utils/catchAsync.js';
+import jwt from 'jsonwebtoken';
 
 export const signUp = catchAsync(async (req, res, next) => {
-	const newUser = await User.create(req.body);
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	});
+
+	const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
 
 	res.status(201).json({
 		status: 'success',
