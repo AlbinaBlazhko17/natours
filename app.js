fc7a66346a5529962bde7d4f6c20ba5c878e8db6
Albinator <albinamivina@gmail.com>
2023-06-27 09:34:42 +0300

    adding rate-limit

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ import globalErrorHandler from './controller/errorController.js';
 import tourRouter from './routes/tourRouter.js';
 import userRouter from './routes/userRouter.js';
 import { AppError } from './utils/appError.js';
+import { rateLimit } from 'express-rate-limit';
 
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many request from this API, please try again in an hour!',
+});
+
+app.user('/api', limiter);
 
 app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
