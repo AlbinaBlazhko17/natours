ec7894f09551a8d53c31ecafd392c219c19b5ff5
Albinator <albinamivina@gmail.com>
2023-07-04 10:56:56 +0300

    add header and footer

--- app.js
+++ app.js
@@ -58,7 +58,10 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-	res.status(200).render('base');
+	res.status(200).render('base', {
+		tour: 'The forest hiker',
+		user: 'Albinator',
+	});
 });
 
 app.use('/api/v1/tours', tourRouter);
