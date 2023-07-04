0f6a1e17647341f62d7992038b5f8e304093bc19
Albinator <albinamivina@gmail.com>
2023-07-04 12:17:13 +0300

    adding cards with tours

--- controller/viewsController.js
+++ controller/viewsController.js
@@ -1,12 +1,17 @@
-export const getOverview = (req, res) => {
-	res.status(200).render('overview', {
-		tour: 'The forest hiker',
-		user: 'Albinator',
-	});
-};
+import Tour from '../models/toursModel.js';
+import catchAsync from '../utils/catchAsync.js';
 
-export const getTour = (req, res) => {
+export const getOverview = catchAsync(async (req, res, next) => {
+	const tours = await Tour.find();
+
+	res.status(200).render('overview', {
+		title: 'All Tours',
+		tours,
+	});
+});
+
+export const getTour = catchAsync(async (req, res, next) => {
 	res.status(200).render('tour', {
 		title: 'All Tours',
 	});
-};
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,13 +3,6 @@ import { getOverview, getTour } from '../controller/viewsController.js';
 
 const viewRouter = express.Router();
 
-// viewRouter.get('/', (req, res) => {
-// 	res.status(200).render('base', {
-// 		tour: 'The forest hiker',
-// 		user: 'Albinator',
-// 	});
-// });
-
 viewRouter.get('/', getOverview);
 viewRouter.get('/tour', getTour);
 
