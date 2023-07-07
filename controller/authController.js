ea28ea85ddae20b0c46c1f385b677f84c82b9f55
Albinator <albinamivina@gmail.com>
2023-07-07 11:34:41 +0300

    add redirecting to main page when logged out

--- controller/authController.js
+++ controller/authController.js
@@ -73,12 +73,14 @@ export const protect = catchAsync(async (req, res, next) => {
 	let token;
 	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
 		token = req.headers.authorization.split(' ')[1];
-	} else if (req.cookies.jwt) {
+	} else if (req.cookies.jwt && req.cookies.jwt !== 'loggedout') {
 		token = req.cookies.jwt;
 	}
 
-	if (!token)
+	if (!token) {
+		res.redirect('/');
 		return next(new AppError('You are not logged in! Please log in to get access.', 401));
+	}
 
 	const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
