f6cc5314591680a0ab065c2780d35bf0ada765a6
Albinator <albinamivina@gmail.com>
2023-07-10 11:21:31 +0300

    functionality for delete photo after uploading new

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,16 @@
+import fs from 'fs';
 import multer from 'multer';
+import path from 'path';
 import sharp from 'sharp';
+import { fileURLToPath } from 'url';
+import util from 'util';
 import User from '../models/userModel.js';
 import { AppError } from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
 import { deleteOne, getAll, getOne, updateOne } from './handlerFactory.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const multerStorage = multer.memoryStorage();
 
@@ -36,6 +43,19 @@ export const resizeUserPhoto = async (req, res, next) => {
 	next();
 };
 
+export const deleteUserPhotoServer = async (photo) => {
+	if (photo.startsWith('default')) return;
+
+	const path = `${__dirname}/../public/img/users/${photo}`;
+	const unlink = util.promisify(fs.unlink);
+
+	try {
+		await unlink(path);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 const filteredObj = (obj, ...allowedFildes) => {
 	const newObj = {};
 	Object.keys(obj).forEach((el) => {
@@ -64,7 +84,10 @@ export const updateMe = catchAsync(async (req, res, next) => {
 		);
 
 	const filteredBody = filteredObj(req.body, 'name', 'email');
-	if (req.file) filteredBody.photo = req.file.filename;
+	if (req.file) {
+		filteredBody.photo = req.file.filename;
+		await deleteUserPhotoServer(req.user.photo);
+	}
 
 	const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
 		new: true,
