b36e0e5bd605f2e982ac681e5a3430558606a23c
Albinator <albinamivina@gmail.com>
2023-07-11 10:29:55 +0300

    building a complex email handler

--- utils/email.js
+++ utils/email.js
@@ -1,22 +1,53 @@
 import nodemailer from 'nodemailer';
+import pug from 'pug';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { htmlToText } from 'html-to-text';
 
-const sendEmail = async (options) => {
-	const transporter = nodemailer.createTransport({
-		host: process.env.EMAIL_HOST,
-		auth: {
-			user: process.env.EMAIL_USERNAME,
-			pass: process.env.EMAIL_PASSWORD,
-		},
-	});
+export const __dirname = path.dirname(fileURLToPath(import.meta.url));
+class Email {
+	constructor(user, url) {
+		this.to = user.email;
+		this.firstName = user.name.split(' ')[0];
+		this.url = url;
+		this.from = `Albina Blazhko <${process.env.EMAIL_FROM}>`;
+	}
 
-	const mailOptions = {
-		from: 'Albina Blazhko <hello@natours.com>',
-		to: options.email,
-		subject: options.email,
-		text: options.message,
-	};
+	createTransport() {
+		if (process.env.NODE_ENV === 'production') {
+			return 1;
+		}
 
-	await transporter.sendMail(mailOptions);
-};
+		return (transporter = nodemailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD,
+			},
+		}));
+	}
 
-export default sendEmail;
+	async send(template, subject) {
+		const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+			firstName: this.firstName,
+			url: this.url,
+			subject,
+		});
+
+		const mailOptions = {
+			from: this.from,
+			to: this.to,
+			subject,
+			html,
+			text: htmlToText.fromString(html),
+		};
+
+		await this.createTransport().sendMail(mailOptions);
+	}
+
+	async sendWelcome() {
+		await this.send('welcome', 'Welcome to the Natours Family!');
+	}
+}
+
+export default Email;
