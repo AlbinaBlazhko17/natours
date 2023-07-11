7c0141aeae48a6d9b16e064ab83e6277d94b7f82
Albinator <albinamivina@gmail.com>
2023-07-11 09:34:37 +0300

    processing multiple images

--- controller/toursController.js
+++ controller/toursController.js
@@ -25,9 +25,35 @@ export const uploadTourImages = upload.fields([
 	{ name: 'images', maxCount: 3 },
 ]);
 
-export const resizeTourImages = (req, res, next) => {
+export const resizeTourImages = catchAsync(async (req, res, next) => {
+	if (!req.files.imageCover || !req.files.images) return next();
+
+	req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+
+	await sharp(req.files.imageCover[0].buffer)
+		.resize(2000, 1333)
+		.toFormat('jpeg')
+		.jpeg({ quality: 90 })
+		.toFile(`public/img/tours/${req.body.imageCover}`);
+
+	req.body.images = [];
+
+	await Promise.all(
+		req.files.images.map(async (file, i) => {
+			const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+			await sharp(req.buffer)
+				.resize(2000, 1333)
+				.toFormat('jpeg')
+				.jpeg({ quality: 90 })
+				.toFile(`public/img/tours/${filename}`);
+
+			req.body.images.push(filename);
+		})
+	);
+
 	next();
-};
+});
 
 export const aliasTopTours = (req, res, next) => {
 	req.query.limit = '5';
