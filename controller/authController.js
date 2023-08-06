0f46f8beac0c706125c51bfd2ba5ec25fe28bb3b
Albinator <albinamivina@gmail.com>
2023-08-06 19:59:46 +0300

    change req.user

--- controller/authController.js
+++ controller/authController.js
@@ -186,6 +186,7 @@ export const isLoggedIn = async (req, res, next) => {
 			}
 
 			// THERE IS A LOGGED IN USER
+			req.user = currentUser;
 			res.locals.user = currentUser;
 			return next();
 		} catch (err) {
